45bddfda73060bbfc43d11846ff1bbc73f2197eb
Zion Emond <zionem7@gmail.com>
2021-02-19 11:06:33 -0500

    better movement system

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # File: main.py
-# Authors: John Cooke
+# Authors: John Cooke, Zion Emond
 # Since: 2/12/2021
 # This file contains the main game loop and will initialize all the game systems
 """
@@ -7,7 +7,6 @@ This module will contain the main high level functions of the game, as well as t
 """
 import logging
 import os
-import time
 
 import pygame
 
@@ -24,16 +23,14 @@ def scale_image(image):
     return pygame.transform.scale(image, (image.get_width()*4, image.get_height()*4))
 
 
-# Load title image
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# title_img = pygame.image.load(os.path.join(current_dir, "Assets", "logo.png"))
-
 # Load images
+current_dir = os.path.dirname(os.path.abspath(__file__))
 background_image = pygame.image.load(os.path.join(current_dir, "Assets", "background.png"))
 background = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
 frog_image = pygame.image.load(os.path.join(current_dir, "Assets", "frog.png"))
 frog = scale_image(frog_image)
+
 MOVEMENT_DISTANCE_X = frog.get_width() + 4
 MOVEMENT_DISTANCE_Y = frog.get_height() + 12
 
@@ -53,23 +50,29 @@ def log_game():
     logging.info("Welcome to The Froggerithm!")
 
 
-def move_player(player):
+def move_player(player, keys_depressed):
     """Handles player movement"""
-    keys_depressed = pygame.key.get_pressed()
+    x_change = 0
+    y_change = 0
 
     if keys_depressed[pygame.K_LEFT] or keys_depressed[pygame.K_a]:  # Left arrow key or a
-        player.x -= MOVEMENT_DISTANCE_X
+        x_change -= MOVEMENT_DISTANCE_X
+        y_change = 0
 
-    if keys_depressed[pygame.K_RIGHT] or keys_depressed[pygame.K_d]:  # Right arrow key or d
-        player.x += MOVEMENT_DISTANCE_X
+    elif keys_depressed[pygame.K_RIGHT] or keys_depressed[pygame.K_d]:  # Right arrow key or d
+        x_change += MOVEMENT_DISTANCE_X
+        y_change = 0
 
-    if keys_depressed[pygame.K_UP] or keys_depressed[pygame.K_w]:  # Up arrow key or w
-        player.y -= MOVEMENT_DISTANCE_Y
+    elif keys_depressed[pygame.K_UP] or keys_depressed[pygame.K_w]:  # Up arrow key or w
+        y_change -= MOVEMENT_DISTANCE_Y
+        x_change = 0
 
-    if keys_depressed[pygame.K_DOWN] or keys_depressed[pygame.K_s]:  # Down arrow key or s
-        player.y += MOVEMENT_DISTANCE_Y
+    elif keys_depressed[pygame.K_DOWN] or keys_depressed[pygame.K_s]:  # Down arrow key or s
+        y_change += MOVEMENT_DISTANCE_Y
+        x_change = 0
 
-    time.sleep(.25)
+    player.x += x_change
+    player.y += y_change
 
 
 def main():
@@ -84,13 +87,15 @@ def main():
     # Main game loop
     while run:
         clock.tick(FPS)
+        draw_window(player)
         for event in pygame.event.get():
 
             if event.type == pygame.QUIT:
                 run = False
 
-        draw_window(player)
-        move_player(player)
+            if event.type == pygame.KEYDOWN:
+                keys_depressed = pygame.key.get_pressed()
+                move_player(player, keys_depressed)
 
 
 if __name__ == "__main__":
