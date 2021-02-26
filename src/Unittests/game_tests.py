0164ae37a61860a81c2be9b92e48cab75b7d77b3
Zion Emond <zionem7@gmail.com>
2021-02-26 16:17:14 -0500

    reworked the movement function to make it easier to test. Wrote a unit test to test the movement function

--- src/Unittests/game_tests.py
+++ src/Unittests/game_tests.py
@@ -15,3 +15,35 @@ class TestGameMethods(unittest.TestCase):
         with self.assertLogs() as logs:
             main.main()
         self.assertEqual(logs.output, ["INFO:root:Welcome to The Froggerithm!"])
+
+    def test_move_player(self):
+        """Test whether the movement system works as expected"""
+        test_player = main.Player(main.asset_dict["frog"])
+        up = 119  # 'w' key ascii
+        left = 97  # 'a' key ascii
+        right = 100  # 'd' key ascii
+        down = 115  # 's' key ascii
+
+        # Test move up
+        test_player.rect.x = 400
+        test_player.rect.y = 400
+        main.move_player(test_player, up)
+        self.assertEqual(test_player.rect.y, 400 - main.MOVEMENT_DISTANCE_Y)
+
+        # Test move left
+        test_player.rect.x = 400
+        test_player.rect.y = 400
+        main.move_player(test_player, left)
+        self.assertEqual(test_player.rect.x, 400 - main.MOVEMENT_DISTANCE_X)
+
+        # Test move right
+        test_player.rect.x = 400
+        test_player.rect.y = 400
+        main.move_player(test_player, right)
+        self.assertEqual(test_player.rect.x, 400 + main.MOVEMENT_DISTANCE_X)
+
+        # Test move down
+        test_player.rect.x = 400
+        test_player.rect.y = 400
+        main.move_player(test_player, down)
+        self.assertEqual(test_player.rect.y, 400 + main.MOVEMENT_DISTANCE_Y)
