a5eb155f334c605cd6d5d0e1359a96746368b976
Zion Emond <zionem7@gmail.com>
2021-03-04 13:38:07 -0500

    player rotates to corresponding inputs as well as has animated jumps

--- src/Engine/movement_handler.py
+++ src/Engine/movement_handler.py
@@ -1,7 +1,9 @@
+import pygame
 from Sprites.player import Player
+from Util.asset_dictionary import AssetDictionary
 
 
-def move_player(player: Player, key_depressed, movement_distance_x, movement_distance_y):
+def move_player(player: Player, key_depressed, movement_distance_x, movement_distance_y, asset_dict):
     """Handles player movement"""
     x_change = 0
     y_change = 0
@@ -10,21 +12,34 @@ def move_player(player: Player, key_depressed, movement_distance_x, movement_dis
     right = 100  # 'd' key ascii
     down = 115  # 's' key ascii
 
+    up_image = asset_dict.get_asset("frog")
+    up_image2 = asset_dict.get_asset("frog_jumping")
+    left_image = pygame.transform.rotate(up_image, 90)
+    left_image2 = pygame.transform.rotate(up_image2, 90)
+    right_image = pygame.transform.rotate(up_image, -90)
+    right_image2 = pygame.transform.rotate(up_image2, -90)
+    down_image = pygame.transform.rotate(up_image, 180)
+    down_image2 = pygame.transform.rotate(up_image2, 180)
+
     if key_depressed == up:  # if up key is pressed
         if player.rect.y > 20:  # not at top
             y_change -= movement_distance_y  # move up
+            player.images = [up_image, up_image2]
 
     elif key_depressed == left:  # if left key is pressed
         if player.rect.x > 20:  # not at leftmost border
             x_change -= movement_distance_x
+            player.images = [left_image, left_image2]
 
     elif key_depressed == right:  # if right key is pressed
         if player.rect.x < 750:  # not at rightmost border
             x_change += movement_distance_x
+            player.images = [right_image, right_image2]
 
     elif key_depressed == down:  # if down key is pressed
         if player.rect.y < 800:  # not at bottom
             y_change += movement_distance_y
+            player.images = [down_image, down_image2]
 
     player.rect.x += x_change
     player.rect.y += y_change
--- src/Sprites/player.py
+++ src/Sprites/player.py
@@ -5,9 +5,13 @@ import pygame.surface
 class Player(pygame.sprite.Sprite):
     """Pygame sprite class representing the player"""
 
-    def __init__(self, image, win: pygame.surface.Surface):
+    def __init__(self, images, win: pygame.surface.Surface):
         pygame.sprite.Sprite.__init__(self)
-        self.image = image
-        self.rect = self.image.get_rect()
+        self.images = images
+        self.rect = self.images[0].get_rect()
         self.rect.x = win.get_width() / 2
-        self.rect.y = win.get_height() - self.rect.height - 9
+        self.rect.y = win.get_height() - self.rect.height - 11
+        self.index = 0
+        self.image = images[self.index]
+        self.direction = "up"
+
